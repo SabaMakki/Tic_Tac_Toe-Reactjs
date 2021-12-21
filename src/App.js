77e8d2b0f1c8d59d551883203223a737125cfeb2
sabamakki <s.sabamakki@gmail.com>
2021-12-21 16:58:07 +0330

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Grid from "@mui/material/Grid";
 import Stack from '@mui/material/Stack';
 import Button from "@mui/material/Button";
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,16 +9,11 @@ import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <div>
-    
-      <Container >
-        <Game />
-      </Container>
-     
+    <div>   
+      <Game />     
     </div>
   )
 }
-
 export default App
 const theme = createTheme({
    palette: {
@@ -27,23 +21,19 @@ const theme = createTheme({
       main: '#C7B198',
     },
     secondary: {
-      main: '#CE97B0',
-     
+      main: '#CE97B0',     
     },
   },
 }); 
 function Square(props) {
   return (
-      <ThemeProvider theme={theme}>
-        <Button className="btn"
-          variant="contained"
-          color="secondary"
+    <ThemeProvider theme={theme}>
+      <Button className="btn" variant="contained" color="secondary"
         disableElevation
-         onClick={props.onClick}>
-         {props.value}
-        </Button>
-      </ThemeProvider>
-   
+        onClick={props.onClick}>
+        {props.value}
+      </Button>
+    </ThemeProvider>   
   );
 }
 
@@ -52,48 +42,43 @@ class Board extends React.Component {
     return (
       <Square 
         value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
+        onClick={() => this.props.onClick(i)} />
     );
   }
   render() {
-     return (
+    return (
       <div>
-         
-
-         <Grid className="board" container direction="column"  justifyContent="center" spacing={1.3} >   
+        <Grid className="board" container direction="column"  justifyContent="center" spacing={1.3} >   
           <Grid item >
             <Stack direction="row"  spacing={1.3}>
-                {this.renderSquare(0)} {this.renderSquare(1)} {this.renderSquare(2)}
+              {this.renderSquare(0)} {this.renderSquare(1)} {this.renderSquare(2)}
             </Stack>            
           </Grid>
           <Grid item>
             <Stack  direction="row" spacing={1.3}>
-                {this.renderSquare(3)} {this.renderSquare(4)} {this.renderSquare(5)}
+              {this.renderSquare(3)} {this.renderSquare(4)} {this.renderSquare(5)}
             </Stack>           
           </Grid>
           <Grid item> 
             <Stack  direction="row"  spacing={1.3}>
-                {this.renderSquare(6)} {this.renderSquare(7)} {this.renderSquare(8)}
+              {this.renderSquare(6)} {this.renderSquare(7)} {this.renderSquare(8)}
             </Stack> 
           </Grid>         
-        </Grid>  
-        
+        </Grid>        
       </div>
     );
   }
 }
 function AppBarr(){
   return(
-<AppBar>
+    <AppBar>
       <Toolbar>
         <Typography variant="h3">
-             Tic Tac Toe
+          Tic Tac Toe
         </Typography>
       </Toolbar>
-   </AppBar>
-  )
-  
+    </AppBar>
+  );  
 }
 class Game extends React.Component {
   constructor(props) {
@@ -104,7 +89,6 @@ class Game extends React.Component {
       }],
       xIsNext: true,
       stepNumber: 0,
-     
     };
   }
 
@@ -125,65 +109,58 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
   }
-
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0,
     });
   }
-
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     
     const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to Step # ' + move :
-        'Start the Game';
+      const desc = move ? 'Go to Step # ' + move : 'Start the Game';
       return (
         <li  className="btn3" key={move}>
-           <ThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
             <Button className="btn2" variant="contained"
               sx={{ backgroundColor: "#CE97B0", fontSize: "15px" }} 
               onClick={() => this.jumpTo(move)}>
               {desc}
            </Button>
-           </ThemeProvider>
-          
+          </ThemeProvider>          
         </li>
       );
     });
 
     let status;
     if (winner === "X" || winner === "O") {
-      status = "** "+ winner+" has Won **";
+      status = "** "+ winner+" has Won **";      
     } else if (current.squares.every(Boolean)) {
       status = "There has been a Draw!!";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
-
     return (
       <Grid container flexDirection={{ xs: "column", sm: "row" }}
-        alignItems={{ xs: "center", sm: "flex-start" }} justifyContent="center" >
-          <Grid item>
-             <ThemeProvider theme={theme}>
-               <AppBarr color="primary"/>
-             </ThemeProvider>
-          </Grid> 
-          <br /><br />
-          <Grid item sm="6" xs="12">
-            <Board
-              squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
-            />
-          </Grid>        
-          <Grid item sm="auto" xs="12" marginTop={{ xs: "2", sm: "0" }}>
-            <div className="status">{status}</div>
-            <ol>{moves}</ol>
-          </Grid>        
+      alignItems={{ xs: "center", sm: "flex-start" }} justifyContent="center" >
+        <Grid item>
+          <ThemeProvider theme={theme}>
+            <AppBarr color="primary"/>
+          </ThemeProvider>
+        </Grid> 
+            <br /><br />
+        <Grid item sm="6" xs="12">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)} />
+        </Grid>        
+        <Grid item sm="auto" xs="12" marginTop={{ xs: "2", sm: "0" }}>
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
+        </Grid>        
       </Grid>
     );
   }
